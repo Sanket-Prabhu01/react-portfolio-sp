f9017c54e6ad600dfcba095ee6346d885ca9c1e3
Sanket Prabhu <soum2022r@gmail.com>
2024-09-17 20:57:41 +0530

    build set

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-portfolio-sp/'  // Replace <repository-name> with your GitHub repo name
+  base: '/'
 
 })
